4a7094713ea54d76a41d117259f73301ee009d7c
“ladanauskaite” <ladanauskaite.juste@gmail.com>
2022-01-06 00:53:44 +0200

    'lol'

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,17 +2,15 @@
 
 namespace App\Exceptions;
 
-use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Support\Arr;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array
+     * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
         //
@@ -21,53 +19,23 @@ class Handler extends ExceptionHandler
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $dontFlash = [
+        'current_password',
         'password',
         'password_confirmation',
     ];
 
     /**
-     * Report or log an exception.
+     * Register the exception handling callbacks for the application.
      *
-     * @param  \Exception  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function register()
     {
-        parent::report($exception);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
-    }
-    
-    
-     protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-        $guard = Arr::get($exception->guards(), 0);
-        
-        switch ($guard) {
-            case 'admin':
-                    return redirect()->guest(route('admin.login'));
-                break;
-            
-            default:
-                    return redirect()->guest(route('login'));
-                break;
-        }
-        
+        $this->reportable(function (Throwable $e) {
+            //
+        });
     }
 }

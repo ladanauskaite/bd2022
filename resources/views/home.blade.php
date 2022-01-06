62f36349894422a72f159bc597debc1e3761729d
“ladanauskaite” <ladanauskaite.juste@gmail.com>
2022-01-06 15:18:31 +0200

    'correction'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,8 @@
   <header class="masthead">
     <div class="container d-flex h-100 align-items-center">
       <div class="mx-auto text-center">
-        <h1 class="mx-auto my-0 text-uppercase">Sportinės veiklos valdymo sistema</h1>
-        <h2 class="text-white-50 mx-auto mt-2 mb-5 text-uppercase">norintiems daugiau</h2>
+        <h1 class="mx-auto my-0 text-uppercase">Sportinės veiklos</h1>
+        <h2 class="text-white-50 mx-auto mt-2 mb-5 text-uppercase">valdymo sistema</h2>
       </div>
     </div>
   </header>
@@ -41,24 +41,6 @@
        </div>
   </section>
 
-<!-- Signup Section -->
-  <section id="signup" class="signup-section">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-10 col-lg-8 mx-auto text-center">
-
-          <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
-          <h2 class="text-white mb-5">Prenumeruoti naujienas!</h2>
-
-          <form class="form-inline d-flex">
-            <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Įvesti el. pašto adresą...">
-            <button type="submit" class="btn btn-primary mx-auto">Prenumeruoti</button>
-          </form>
-
-        </div>
-      </div>
-    </div>
-  </section>
 
   <!-- Contact Section -->
   <section class="contact-section bg-black">
--- resources/views/user/kainorastis.blade.php
+++ resources/views/user/kainorastis.blade.php
@@ -67,24 +67,7 @@
   
   
   
-  <!-- Signup Section -->
-  <section id="signup" class="signup-section">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-10 col-lg-8 mx-auto text-center">
-
-          <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
-          <h2 class="text-white mb-5">Prenumeruoti naujienas!</h2>
-
-          <form class="form-inline d-flex">
-            <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Įvesti el. pašto adresą...">
-            <button type="submit" class="btn btn-primary mx-auto">Prenumeruoti</button>
-          </form>
-
-        </div>
-      </div>
-    </div>
-  </section>
+ 
 
   <!-- Contact Section -->
   <section class="contact-section bg-black">
